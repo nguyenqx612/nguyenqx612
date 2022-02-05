836b0868357c976c292f1ffd70addb8e74d2caf0
Quan Nguyen <nguyenqx612@gmail.com>
2022-02-04 19:04:39 -0500

    v2

--- Python/Password-Gen.py
+++ Python/Password-Gen.py
@@ -1,6 +1,9 @@
 import string
 import random
 
+from numpy import number
+from sympy import numbered_symbols
+
 letters = list(string.ascii_letters)
 numbers = list(string.digits)
 specialCharacters = list("!@#$%^&*()")
@@ -14,11 +17,28 @@ def generate_password():
     number_count = int(input("Please enter the number of digits you would like in your password: "))
     special_Characters_count = int(input("Please enter the number of special characters you would like in your password: "))
 
-    random.shuffle(specialCharacters)
+    characters_count = letters_count + number_count + special_Characters_count
+
+    if (characters_count > passwordLength)
+        print("Characters total count is greater than the password length.")
+        return
 
     password = []
-    for i in range(passwordLength):
+    
+    for i in range(letters_count):
+        password.append(random.choice(letters))
+
+    for i in range(number_count):
+        password.append(random.choice(numbers))
+    
+    for i in range(specialCharacters):
         password.append(random.choice(specialCharacters))
+
+
+    if characters_count < passwordLength:
+        random.shuffle(characters)
+        for i in range(passwordLength - characters_count):
+            password.append(random.choice(characters))
 
     random.shuffle(password)
 
